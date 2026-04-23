2b2c1feaee6c0fe5b0c2725f049a9e5ebc08c8d9
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-23 17:20:53 -0600

    first challenge - tiny self presentation

---
+++ practica-java/src/Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    static void main() {
+        System.out.print("Nombre completo: Luis Guillermo Orozco Lazo" +
+                "\nEdad: 24 años" +
+                "\nPaís de origen: Nicaragua");
+    }
+}
