798be3b3ce9a560efb6290057938efe6b232c7f7
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-23 21:32:38 -0600

    string concatenations and const variables

---
+++ practica-java/src/ConcatenacionCadenas.java
@@ -0,0 +1,9 @@
+public class ConcatenacionCadenas {
+    static void main() {
+        System.out.println("***Concatenacios de Cadenas***");
+        var nombre = "Alexa";
+        var apellido = "Cruz";
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("Nombre completo = " + nombreCompleto);
+    }
+}
---
+++ practica-java/src/Constantes.java
@@ -0,0 +1,18 @@
+public class Constantes {
+    static void main() {
+        /*Las constantes se declaran con la palabra reservada final, luego el tipo de variable
+        Y por ultimo el nombre de la variable completamente en mayuscula y separado por guines bajos
+        Cabe a destacar que se declara su valor en el momento y no se puede cambiar del todo
+        */
+        System.out.println("*** Constantes en Java ***");
+        final var DIAS_SEMANA = 7;
+        System.out.println("DIAS_SEMANA = " + DIAS_SEMANA);
+        final var PI = 3.1416;
+        System.out.println("PI = " + PI);
+        System.out.println("Math.PI: " + Math.PI);
+        final var MENSAJE_BIENVENIDA = "Bienvenid@ a la universidad Java";
+        System.out.println("MENSAJE_BIENVENIDA = " + MENSAJE_BIENVENIDA);
+        final var MINUTOS_POR_SEGUNDO = 60;
+        System.out.println("MINUTOS_POR_SEGUNDO = " + MINUTOS_POR_SEGUNDO);
+    }
+}
