78fe00a5f14ec4fe7e44f9546b5b4722b7c28e86
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-23 20:47:05 -0600

    third challenge

---
+++ practica-java/src/DetalleProducto.java
@@ -0,0 +1,25 @@
+public class DetalleProducto {
+    static void main() {
+        //Tercer reto del curso toca poner variables y definirlas, luego cambiar el valor de cada una
+        //se imprimen 2 veces
+        String nombreProducto = "Churritos Diana";
+        double precioProducto = 10.5;
+        int cantidadDisponible = 25;
+        boolean estaDisponible = true;
+        System.out.println("Nombre del Producto: " + nombreProducto + "\n" +
+                "Precio: " + precioProducto + "\n" +
+                "Cantidad disponible: " + cantidadDisponible + "m\n" +
+                "Disponibbilidad: " + estaDisponible + "\n");
+
+        nombreProducto = "Jalapeños Diana";
+        precioProducto = 10.0;
+        cantidadDisponible = 0;
+        estaDisponible = false;
+
+        System.out.println("Nombre del Producto: " + nombreProducto + "\n" +
+                "Precio: " + precioProducto + "\n" +
+                "Cantidad disponible: " + cantidadDisponible + "m\n" +
+                "Disponibbilidad: " + estaDisponible + "\n");
+
+    }
+}
