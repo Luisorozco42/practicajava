8db77ec3a1770d7565d6d93fd4e112f0c6e86fdd
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-23 19:44:58 -0600

    second challenge

---
+++ practica-java/src/DetallePersona.java
@@ -0,0 +1,17 @@
+public class DetallePersona {
+    static void main() {
+        // Este es el segundo reto propuesto en el curso
+        String nombreCompleto = "Luis Guillermo Orozco Lazo";
+        int edad = 24;
+        double altura = 1.78;
+        String paisOrigen = "Nicaragua";
+        char estadoCivil = 'S';
+
+        System.out.println("Nombre completo: " + nombreCompleto + "\n" +
+                "Edad: " + edad + "\n" +
+                "Altura: " + altura + "m\n" +
+                "Pais de Origen: " + paisOrigen + "\n" +
+                "Estado Civil: " + estadoCivil);
+
+    }
+}
