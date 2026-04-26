e2bd337f7ac89fa0d13b1cdaaad0cb50b615db0b
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-25 23:24:07 -0600

    another type of string format

--- practica-java/src/formatoCadena/FormateoCadena.java
+++ practica-java/src/formatoCadena/FormateoCadena.java
@@ -9,11 +9,33 @@ public class FormateoCadena {
         var salario = 21000.50;
 
         //String.format
-        var mensaje = String.format("Nombre: %s Edad: %d Salario:  %.2f", nombre, edad, salario);
+        var mensaje = String.format("Nombre: %s Edad: %d Salario:  $%.2f", nombre, edad, salario);
         System.out.println(mensaje);
 
         //otra forma
-        System.out.printf("Nombre: %s, Edad: %d, Salario: %.2f", nombre, edad, salario);
+        System.out.printf("Nombre: %s, Edad: %d, Salario: $%.2f%n", nombre, edad, salario);
+
+        var numeroEmpleado = 12;
+        //formateo text block el \s es un espacio en blanco
+        mensaje = """
+                %nDetalle Persona:\s 
+                ---------------
+                \tNombre: %s
+                \tNo. Empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """.formatted(nombre, numeroEmpleado, edad, salario);
+        System.out.println(mensaje);
+
+        //formateo con text block y printf
+        System.out.printf("""
+                %nDetalle Persona:\s 
+                ---------------
+                \tNombre: %s
+                \tNo. Empleado: %04d
+                \tEdad: %d años
+                \tSalario: $%.2f
+                """, nombre, numeroEmpleado, edad, salario);
 
     }
 }
