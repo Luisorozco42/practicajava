8e6c7329ec437184b652870bfc4a171b12efc198
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-26 16:33:02 -0600

    end of operators

---
+++ practica-java/src/operadores/PrecedenciaOperadores.java
@@ -0,0 +1,20 @@
+package operadores;
+
+public class PrecedenciaOperadores {
+    static void main() {
+        //Precedencia operadores en Java
+        //1. Parentesis y corchetes
+        //2. Operadores Unarios --, ++ y -
+        //3. Operadores aritmeticos *, / y %
+        //4. Operadores aritmeticos + y -
+        //5. Relacionales <, <=, > y >=
+        //6. Equivalencia == y !=
+        //7. Logicos && y ||
+        //8. Asignacion =, +=, -=, *=, etc
+
+        //Expresion
+        var a = 12 / 3 + 2 * 3 - 1;
+        System.out.println("a = " + a);
+
+    }
+}
